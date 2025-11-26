19970bfd7deead353eb73cea33dd12d25fb39062
Vijay2278-ux <peddintivijay755@gmail.com>
2025-11-27 00:31:28 +0530

    lang

---
+++ v0-code-my---new-main/lib/v.code.py
@@ -0,0 +1,15 @@
+a =("hello am vijay here is ther elogest letters ")  
+b = a.split()
+max = 0
+maxs = 0
+for i in b:
+    if(i > max ):
+        maxs = max 
+        max = i
+
+print(f"{maxs}\n{max}")       
+
+
+
+
+                
